c5ab3f6603d46c64f4334502a97496f20c9ccb32
GitHub <noreply@github.com>
2021-03-11 17:30:51 +0000

    068dd44 chore(deps): bump @dhis2/app-runtime from 2.7.0 to 2.8.0

---
+++ static/js/data-visualizer-plugin.e101203b.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{1948:function(p,s){}}]);
+//# sourceMappingURL=data-visualizer-plugin.e101203b.chunk.js.map
