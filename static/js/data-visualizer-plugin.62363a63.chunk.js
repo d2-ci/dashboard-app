300e14480693b33ecfb4393f19619fe680c43bac
GitHub <noreply@github.com>
2021-03-11 23:09:31 +0000

    662fd6e fix: upgrade to @dhis2/cli-app-scripts@6 (DHIS2-9893) (#1634)

--- static/js/data-visualizer-plugin.62363a63.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{1938:function(p,s){}}]);
-//# sourceMappingURL=data-visualizer-plugin.62363a63.chunk.js.map
