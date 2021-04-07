59462e4583108acb57abcaff6e7e8f7654c0b545
Jen Jones Arnesen <jennifer@dhis2.org>
2021-04-07 08:45:13 +0000

    c04d34d Merge branch 'master' into chore/restructure-folders

--- static/js/data-visualizer-plugin.337b6f01.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{1979:function(p,s){}}]);
-//# sourceMappingURL=data-visualizer-plugin.337b6f01.chunk.js.map
