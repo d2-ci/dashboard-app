c054c2c8a66ae46293c05c6638b9a7943188ca17
Edoardo Sabadelli <edoardo@dhis2.org>
2021-04-19 10:01:16 +0000

    4a3612b refactor: use local icons to fix failing tests

---
+++ static/js/data-visualizer-plugin.83cc0adb.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{2146:function(p,s){}}]);
+//# sourceMappingURL=data-visualizer-plugin.83cc0adb.chunk.js.map
