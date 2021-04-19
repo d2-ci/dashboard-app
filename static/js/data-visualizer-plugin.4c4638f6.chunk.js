34cc0b40288c9eaafb3e73b8c648c84ec6e76c31
Edoardo Sabadelli <edoardo@dhis2.org>
2021-04-19 12:44:23 +0000

    0b6f618 chore: update analytics dependency

---
+++ static/js/data-visualizer-plugin.4c4638f6.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{2155:function(p,s){}}]);
+//# sourceMappingURL=data-visualizer-plugin.4c4638f6.chunk.js.map
--- static/js/data-visualizer-plugin.83cc0adb.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{2146:function(p,s){}}]);
-//# sourceMappingURL=data-visualizer-plugin.83cc0adb.chunk.js.map
