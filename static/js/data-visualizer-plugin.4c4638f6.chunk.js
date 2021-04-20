85097a02c6ced374d3c9a12e884fea9c187e3303
Edoardo Sabadelli <edoardo@dhis2.org>
2021-04-20 08:28:44 +0000

    99c1547 fix: pass insertTheme to d2-ui components to fix narrow dialogs

--- static/js/data-visualizer-plugin.4c4638f6.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{2155:function(p,s){}}]);
-//# sourceMappingURL=data-visualizer-plugin.4c4638f6.chunk.js.map
