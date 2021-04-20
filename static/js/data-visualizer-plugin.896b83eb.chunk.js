2a4cd198448bb1e79e3bf2510aa068b489ae26b4
GitHub <noreply@github.com>
2021-04-20 12:45:26 +0000

    4f4cc50 fix: remove material-ui as direct dependency and update to latest ui design DHIS2-10143 (#1704)

--- static/js/data-visualizer-plugin.896b83eb.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{1978:function(p,s){}}]);
-//# sourceMappingURL=data-visualizer-plugin.896b83eb.chunk.js.map
