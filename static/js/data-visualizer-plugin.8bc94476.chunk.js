9231ab86f9e6db2d5b126a57ba68a8d5ad6bef05
GitHub <noreply@github.com>
2021-04-22 16:59:59 +0000

    e98143e chore(deps): bump @dhis2/ui from 6.6.1 to 6.6.2

---
+++ static/js/data-visualizer-plugin.8bc94476.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{2469:function(p,s){}}]);
+//# sourceMappingURL=data-visualizer-plugin.8bc94476.chunk.js.map
