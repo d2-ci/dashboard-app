5af0002404d473acd6ae2ceb40ee896371d67e04
GitHub <noreply@github.com>
2021-04-27 09:54:44 +0000

    0997714 fix: namespace check issue (#1725)

---
+++ static/js/data-visualizer-plugin.7c154ad1.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{2572:function(p,s){}}]);
+//# sourceMappingURL=data-visualizer-plugin.7c154ad1.chunk.js.map
