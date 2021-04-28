af53821f71831975a2ca75b134c50432887f4bb7
GitHub <noreply@github.com>
2021-04-28 09:03:04 +0000

    b48d1ec Merge branch 'master' into dependabot/npm_and_yarn/master/react-redux-7.2.4

--- static/js/data-visualizer-plugin.7c154ad1.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{2572:function(p,s){}}]);
-//# sourceMappingURL=data-visualizer-plugin.7c154ad1.chunk.js.map
