cdbdfff7fa0cdd2a05db0d32de78f05b4e43ccb7
GitHub <noreply@github.com>
2021-04-28 11:39:41 +0000

    a0494f5 Merge branch 'master' into dependabot/npm_and_yarn/master/ssri-6.0.2

---
+++ static/js/data-visualizer-plugin.bb8fc1ce.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{2525:function(p,s){}}]);
+//# sourceMappingURL=data-visualizer-plugin.bb8fc1ce.chunk.js.map
