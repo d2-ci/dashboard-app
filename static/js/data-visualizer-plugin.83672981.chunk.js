03d1ae8395ddc1101fbec944252a0800d6ade6a1
GitHub <noreply@github.com>
2021-04-29 14:09:35 +0000

    72ce120 Merge branch 'master' into dependabot/npm_and_yarn/master/dhis2/data-visualizer-plugin-37.0.1

---
+++ static/js/data-visualizer-plugin.83672981.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{2497:function(p,s){}}]);
+//# sourceMappingURL=data-visualizer-plugin.83672981.chunk.js.map
