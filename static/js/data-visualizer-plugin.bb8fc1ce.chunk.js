caa3c3ab97c936093cde8ec3797e54b5f43ca89d
GitHub <noreply@github.com>
2021-05-06 13:51:53 +0000

    96391b9 chore(deps): bump d2 from 31.9.3 to 31.10.0

--- static/js/data-visualizer-plugin.bb8fc1ce.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{2525:function(p,s){}}]);
-//# sourceMappingURL=data-visualizer-plugin.bb8fc1ce.chunk.js.map
