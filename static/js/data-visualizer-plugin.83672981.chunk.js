391dea4ed66ab0e91bed14f49942bbf12323f0a7
GitHub <noreply@github.com>
2021-05-06 07:48:48 +0000

    971b2b9 fix: sync translations from transifex (master) (#1701)

--- static/js/data-visualizer-plugin.83672981.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{2497:function(p,s){}}]);
-//# sourceMappingURL=data-visualizer-plugin.83672981.chunk.js.map
