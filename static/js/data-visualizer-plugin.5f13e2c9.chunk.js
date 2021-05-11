03224088d4e1672755444978f9557974d5d5ebff
Jen Jones Arnesen <jennifer@dhis2.org>
2021-05-11 07:16:11 +0000

    a87b4b7 fix: updating rows needs to happen in an effect

---
+++ static/js/data-visualizer-plugin.5f13e2c9.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{3056:function(p,s){}}]);
+//# sourceMappingURL=data-visualizer-plugin.5f13e2c9.chunk.js.map
