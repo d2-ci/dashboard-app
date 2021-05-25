70182c10410b486b15236dbc675a6cc682d80012
GitHub <noreply@github.com>
2021-05-25 07:30:58 +0000

    920b5fe chore(deps): bump @dhis2/d2-ui-core from 7.2.0 to 7.3.0

---
+++ static/js/data-visualizer-plugin.66d9cfb1.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{2532:function(p,s){}}]);
+//# sourceMappingURL=data-visualizer-plugin.66d9cfb1.chunk.js.map
