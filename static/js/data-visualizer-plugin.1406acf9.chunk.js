25f02a4ae83bd251ef14552ba60605a499e3516e
GitHub <noreply@github.com>
2021-05-25 09:36:32 +0000

    cbf7740 chore(deps): bump @dhis2/d2-ui-rich-text from 7.2.0 to 7.3.0

--- static/js/data-visualizer-plugin.1406acf9.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{3060:function(p,s){}}]);
-//# sourceMappingURL=data-visualizer-plugin.1406acf9.chunk.js.map
