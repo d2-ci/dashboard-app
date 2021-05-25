d710b52605071013c3649ad7c8f0a57114bec27e
Jen Jones Arnesen <jennifer@dhis2.org>
2021-05-25 11:04:22 +0000

    86a7726 fix: merge conflict fix

--- static/js/data-visualizer-plugin.5f13e2c9.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{3056:function(p,s){}}]);
-//# sourceMappingURL=data-visualizer-plugin.5f13e2c9.chunk.js.map
