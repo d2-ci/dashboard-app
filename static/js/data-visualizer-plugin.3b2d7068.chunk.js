f04f8a0f0bbdecb26e18a60083cb1c60ed171c10
Jen Jones Arnesen <jennifer@dhis2.org>
2021-05-26 10:39:25 +0000

    378907a fix: add timeout

---
+++ static/js/data-visualizer-plugin.3b2d7068.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{3055:function(p,s){}}]);
+//# sourceMappingURL=data-visualizer-plugin.3b2d7068.chunk.js.map
