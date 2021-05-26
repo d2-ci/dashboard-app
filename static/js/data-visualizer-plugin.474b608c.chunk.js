1e2a243b51fc32f86630540b1f1fb6037849c495
GitHub <noreply@github.com>
2021-05-26 07:49:52 +0000

    8a5250b chore(deps): bump @dhis2/d2-ui-sharing-dialog from 7.2.0 to 7.3.0

---
+++ static/js/data-visualizer-plugin.474b608c.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{2502:function(p,s){}}]);
+//# sourceMappingURL=data-visualizer-plugin.474b608c.chunk.js.map
