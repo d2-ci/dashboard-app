a3f51a757283a7f77d9f5deff202f0d330099a9a
GitHub <noreply@github.com>
2021-05-26 10:40:43 +0000

    8469df2 Merge branch 'master' into chore/cypress-refactor

--- static/js/data-visualizer-plugin.3b2d7068.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{3055:function(p,s){}}]);
-//# sourceMappingURL=data-visualizer-plugin.3b2d7068.chunk.js.map
