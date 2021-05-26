534aa7f4d8263c92a2d4dea1303dc2f1bf429933
GitHub <noreply@github.com>
2021-05-26 07:52:17 +0000

    bc8bcae chore(deps): bump @dhis2/analytics from 17.0.5 to 17.1.0

---
+++ static/js/data-visualizer-plugin.1112fa08.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{2491:function(p,s){}}]);
+//# sourceMappingURL=data-visualizer-plugin.1112fa08.chunk.js.map
