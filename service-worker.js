d533f6c6f41a88fc89e97551fe3ab1e9017d8db8
GitHub <noreply@github.com>
2021-06-02 07:44:20 +0000

    907ae93 fix: avoid loading user/groups in interpretations DHIS2-10625 v33 (#1799)

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.5c6c16fc4d68c01b3298dea610eb7126.js"
+  "./precache-manifest.2cd61993299606f8387b252954021cc0.js"
 );
 
 workbox.clientsClaim();
