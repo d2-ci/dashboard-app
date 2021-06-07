a97e7ec757114c5a8477faad9ef47095fef5ed66
GitHub <noreply@github.com>
2021-06-07 08:18:52 +0000

    8465154 chore(deps): bump @dhis2/data-visualizer-plugin from 37.3.1 to 37.4.3 (#1807)

--- static/js/data-visualizer-plugin.e3e1220e.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{2479:function(p,s){}}]);
-//# sourceMappingURL=data-visualizer-plugin.e3e1220e.chunk.js.map
