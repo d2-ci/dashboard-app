5888fccaa2ad2945a8c2d1d3a2316aaa3378ffd8
GitHub <noreply@github.com>
2021-06-09 13:46:54 +0000

    48810b9 Merge branch 'master' into feat/auto-layout

--- static/js/data-visualizer-plugin.bea471bf.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{2442:function(p,s){}}]);
-//# sourceMappingURL=data-visualizer-plugin.bea471bf.chunk.js.map
