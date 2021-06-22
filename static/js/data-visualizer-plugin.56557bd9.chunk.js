7cb47f8343bc6e580c7488853952162bb39db103
GitHub <noreply@github.com>
2021-06-22 10:28:26 +0000

    2aa5f6b fix: add onError callback to handle failed chart from DV plugin [DHIS2-11303] (#1826)

--- static/js/data-visualizer-plugin.56557bd9.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{2451:function(p,s){}}]);
-//# sourceMappingURL=data-visualizer-plugin.56557bd9.chunk.js.map
