264a98cf525d2e52cf3cd22d7a9a24c1ff7b0118
Edoardo Sabadelli <edoardo@dhis2.org>
2021-06-23 09:54:03 +0000

    228d5fe fix: fix locale format passed to Intl in interpretations

---
+++ static/js/data-visualizer-plugin.5267365a.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{2499:function(p,s){}}]);
+//# sourceMappingURL=data-visualizer-plugin.5267365a.chunk.js.map
