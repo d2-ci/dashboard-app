1857d9f99a9455252edb2088126ca579f3d2cecd
Edoardo Sabadelli <edoardo@dhis2.org>
2021-06-23 12:34:05 +0000

    f47a261 chore: bump version to all d2-ui dependencies

--- static/js/data-visualizer-plugin.5267365a.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{2499:function(p,s){}}]);
-//# sourceMappingURL=data-visualizer-plugin.5267365a.chunk.js.map
