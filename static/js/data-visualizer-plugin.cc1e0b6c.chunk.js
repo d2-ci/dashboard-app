94b206c7194ad33eba97fa7583f12f982a59f2f9
Jen Jones Arnesen <jennifer@dhis2.org>
2021-06-30 10:21:05 +0000

    f3b0903 Merge branch 'master' into dependabot/npm_and_yarn/master/dhis2/data-visualizer-plugin-37.5.0

---
+++ static/js/data-visualizer-plugin.cc1e0b6c.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{3114:function(p,s){}}]);
+//# sourceMappingURL=data-visualizer-plugin.cc1e0b6c.chunk.js.map
