3cb84d17e3553c90ac1977132756fb99ccef7dc4
Jen Jones Arnesen <jennifer@dhis2.org>
2021-06-30 07:20:07 +0000

    2a8337e fix: more snapshot updates

---
+++ static/js/data-visualizer-plugin.e96fb058.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{2463:function(p,s){}}]);
+//# sourceMappingURL=data-visualizer-plugin.e96fb058.chunk.js.map
