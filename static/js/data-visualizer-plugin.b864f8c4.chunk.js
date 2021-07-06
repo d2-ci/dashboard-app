4f16bf839b76e5f0aa18de4ff83c06405c8ff181
GitHub <noreply@github.com>
2021-07-06 15:52:41 +0000

    8bb0588 chore(deps): bump @dhis2/analytics from 19.0.3 to 20.0.2

---
+++ static/js/data-visualizer-plugin.b864f8c4.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{3085:function(p,s){}}]);
+//# sourceMappingURL=data-visualizer-plugin.b864f8c4.chunk.js.map
