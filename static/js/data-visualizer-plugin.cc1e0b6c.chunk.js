5e9fe1babe53c07401f85b5b1a2e9cc496f11d69
GitHub <noreply@github.com>
2021-07-09 12:12:08 +0000

    9595957 Merge branch 'master' into fix/style-change-vis-error-msg

--- static/js/data-visualizer-plugin.cc1e0b6c.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{3114:function(p,s){}}]);
-//# sourceMappingURL=data-visualizer-plugin.cc1e0b6c.chunk.js.map
