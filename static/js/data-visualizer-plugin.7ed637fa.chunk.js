18edc4d9bc4b43d892fec1704a87bba87f7c90f5
Jen Jones Arnesen <jennifer@dhis2.org>
2021-08-12 09:30:29 +0000

    21f3729 fix: cleanup

---
+++ static/js/data-visualizer-plugin.7ed637fa.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{3089:function(p,s){}}]);
+//# sourceMappingURL=data-visualizer-plugin.7ed637fa.chunk.js.map
