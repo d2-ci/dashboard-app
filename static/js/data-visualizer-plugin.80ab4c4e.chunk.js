df79cc3098e7e4c11a8ec9d9b056cdbd747d827d
Edoardo Sabadelli <edoardo@dhis2.org>
2021-08-13 13:52:36 +0000

    bdfaf22 chore: use analytics@20.1.0-alpha.2 for testing

---
+++ static/js/data-visualizer-plugin.80ab4c4e.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{3118:function(p,s){}}]);
+//# sourceMappingURL=data-visualizer-plugin.80ab4c4e.chunk.js.map
--- static/js/data-visualizer-plugin.e3f45d01.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{3115:function(p,s){}}]);
-//# sourceMappingURL=data-visualizer-plugin.e3f45d01.chunk.js.map
