122d0e73be2524840b91d567e62096e56ca51f9b
GitHub <noreply@github.com>
2021-08-13 12:46:37 +0000

    fbc0881 Merge branch 'master' into master-transifex-ALL-20210630_214120

--- static/js/data-visualizer-plugin.1b45a01f.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{3086:function(p,s){}}]);
-//# sourceMappingURL=data-visualizer-plugin.1b45a01f.chunk.js.map
