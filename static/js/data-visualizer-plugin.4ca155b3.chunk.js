03c01a428808810373070dae3942f9ee5d4c91b4
GitHub <noreply@github.com>
2021-08-26 08:59:21 +0000

    bf3c61b fix: legend key print style (DHIS2-11636) (#1911)

--- static/js/data-visualizer-plugin.4ca155b3.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{2474:function(p,s){}}]);
-//# sourceMappingURL=data-visualizer-plugin.4ca155b3.chunk.js.map
