ac522cbcea13ec2bbb1442490f5d57b3facc8e16
Martin Ohlson <martin@moid.se>
2021-08-26 08:22:56 +0000

    61efbe6 chore: cleanup yarn.lock

--- static/js/data-visualizer-plugin.d3169b8d.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{3088:function(p,s){}}]);
-//# sourceMappingURL=data-visualizer-plugin.d3169b8d.chunk.js.map
