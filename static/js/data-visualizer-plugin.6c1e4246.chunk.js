55cf6612ccc93f16ceba95445fd88ba37a8ce848
Jen Jones Arnesen <jennifer@dhis2.org>
2021-08-31 10:18:12 +0000

    73cbbcf fix: test fix

--- static/js/data-visualizer-plugin.6c1e4246.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{3212:function(p,s){}}]);
-//# sourceMappingURL=data-visualizer-plugin.6c1e4246.chunk.js.map
---
+++ static/js/data-visualizer-plugin.e8fff6a4.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{3213:function(p,s){}}]);
+//# sourceMappingURL=data-visualizer-plugin.e8fff6a4.chunk.js.map
