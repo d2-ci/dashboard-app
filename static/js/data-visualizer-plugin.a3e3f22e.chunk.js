9e171d2e82c01a654d7667830e9c539d5ce665e0
GitHub <noreply@github.com>
2021-09-01 10:24:58 +0000

    264a999 fix: disable or delete cypress tests that are currently broken [v37] (#1925) (#1926)

---
+++ static/js/data-visualizer-plugin.a3e3f22e.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{3105:function(p,s){}}]);
+//# sourceMappingURL=data-visualizer-plugin.a3e3f22e.chunk.js.map
