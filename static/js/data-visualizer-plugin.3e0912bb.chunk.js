5be51cd4694a95c487299e061f020285957f9e1b
Martin Ohlson <martin@moid.se>
2021-09-01 13:52:02 +0000

    1fc61b2 chore: merge branch 'master' into feat/new-sharing-dialog

--- static/js/data-visualizer-plugin.3e0912bb.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{3132:function(p,s){}}]);
-//# sourceMappingURL=data-visualizer-plugin.3e0912bb.chunk.js.map
