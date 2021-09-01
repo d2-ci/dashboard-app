ddba8172f7600e55f81b15fa811d1660e3aa5b6c
GitHub <noreply@github.com>
2021-09-01 14:05:31 +0000

    0d37a41 feat: use the new SharingDialog with cascading sharing (#1913)

---
+++ static/js/data-visualizer-plugin.24cd6138.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{3103:function(p,s){}}]);
+//# sourceMappingURL=data-visualizer-plugin.24cd6138.chunk.js.map
