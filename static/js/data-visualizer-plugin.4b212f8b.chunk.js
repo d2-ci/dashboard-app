c13d0851c3ab1a1358371f7b34e764da628df85b
GitHub <noreply@github.com>
2021-09-01 14:12:21 +0000

    d913346 Merge branch 'master' into fix/cypress-tests-fix-1

---
+++ static/js/data-visualizer-plugin.4b212f8b.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{3106:function(p,s){}}]);
+//# sourceMappingURL=data-visualizer-plugin.4b212f8b.chunk.js.map
