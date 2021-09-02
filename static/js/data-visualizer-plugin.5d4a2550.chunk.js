852604d0524298b543474f8bbe7bcc8405786326
GitHub <noreply@github.com>
2021-09-02 13:34:47 +0000

    5ec2fbc Merge branch 'master' into fix/bump-cli-app-scripts

---
+++ static/js/data-visualizer-plugin.5d4a2550.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{3104:function(p,s){}}]);
+//# sourceMappingURL=data-visualizer-plugin.5d4a2550.chunk.js.map
