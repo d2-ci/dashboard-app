8947c4e3393db0013de15937b606d702360fae5e
GitHub <noreply@github.com>
2021-09-03 09:40:45 +0000

    4e26558 fix: bump analytics and ui to latest (#1927) (#1934)

--- static/js/data-visualizer-plugin.a3e3f22e.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{3105:function(p,s){}}]);
-//# sourceMappingURL=data-visualizer-plugin.a3e3f22e.chunk.js.map
