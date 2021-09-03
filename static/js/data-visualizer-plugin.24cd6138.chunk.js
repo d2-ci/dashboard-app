1b7d63081fc41fc16364aa0554a093d620974046
GitHub <noreply@github.com>
2021-09-03 09:29:10 +0000

    9c6a2e3 fix: reduce cypress runners from 10 to 3 for fewer db conflicts (#1937)

--- static/js/data-visualizer-plugin.24cd6138.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{3103:function(p,s){}}]);
-//# sourceMappingURL=data-visualizer-plugin.24cd6138.chunk.js.map
