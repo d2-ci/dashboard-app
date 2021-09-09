35817e28433d749695cf6ed4cbfc4f61b0b142fb
Martin Ohlson <martin@moid.se>
2021-09-09 10:59:20 +0000

    6e95d71 fix: bump deps, revert dv plugin to 37.x.x

---
+++ static/js/data-visualizer-plugin.28ea27b0.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{2509:function(p,s){}}]);
+//# sourceMappingURL=data-visualizer-plugin.28ea27b0.chunk.js.map
