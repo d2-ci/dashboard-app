8b9be1b44da950afeb43866aaf04e36d9d2bf338
GitHub <noreply@github.com>
2021-09-11 12:36:11 +0000

    115c694 Merge branch 'v37' into fix/i18n-plurals-for-layout-columns-v37

--- static/js/data-visualizer-plugin.28ea27b0.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{2509:function(p,s){}}]);
-//# sourceMappingURL=data-visualizer-plugin.28ea27b0.chunk.js.map
