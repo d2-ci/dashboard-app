38d66b131ef2415addc976450c9799a623dc5a1f
Jen Jones Arnesen <jennifer@dhis2.org>
2021-09-28 11:54:22 +0000

    83bb454 fix: enable Print if dashboard is cached

---
+++ static/js/data-visualizer-plugin.843fa0be.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{2489:function(p,s){}}]);
+//# sourceMappingURL=data-visualizer-plugin.843fa0be.chunk.js.map
