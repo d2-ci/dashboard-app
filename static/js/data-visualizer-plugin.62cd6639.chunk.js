22048146376866b21db52ab22f97a5ea56f1c9cc
GitHub <noreply@github.com>
2021-10-07 13:37:40 +0000

    2d82c03 fix: bump dv plugin to latest (#2030)

--- static/js/data-visualizer-plugin.62cd6639.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{2330:function(p,s){}}]);
-//# sourceMappingURL=data-visualizer-plugin.62cd6639.chunk.js.map
