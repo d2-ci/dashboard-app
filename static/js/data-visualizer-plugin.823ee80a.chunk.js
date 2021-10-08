6bc6f226631a9b0f657a17bbe546841b06c73629
Martin Ohlson <martin@moid.se>
2021-10-08 10:12:17 +0000

    a650d85 fix: bump Analytics to latest 16.x.x

---
+++ static/js/data-visualizer-plugin.823ee80a.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{2911:function(p,s){}}]);
+//# sourceMappingURL=data-visualizer-plugin.823ee80a.chunk.js.map
