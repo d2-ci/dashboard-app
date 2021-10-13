ce1c45d638fd0acb3f617d4172e9412ef235d723
GitHub <noreply@github.com>
2021-10-13 08:20:42 +0000

    2da81a4 Merge branch 'v37' into v37-transifex-ALL-20211006_232557

---
+++ static/js/data-visualizer-plugin.fc84ae32.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{2490:function(p,s){}}]);
+//# sourceMappingURL=data-visualizer-plugin.fc84ae32.chunk.js.map
