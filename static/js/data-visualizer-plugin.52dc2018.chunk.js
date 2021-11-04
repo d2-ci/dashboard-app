97ca88d66164ba2c1463f27c57bea3558fcf4519
GitHub <noreply@github.com>
2021-11-04 16:01:46 +0000

    7200ca3 Merge branch 'patch/2.37.0' into origin/bump-platform-deps-DHIS2-11976-2.37.0

--- static/js/data-visualizer-plugin.52dc2018.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{2491:function(p,s){}}]);
-//# sourceMappingURL=data-visualizer-plugin.52dc2018.chunk.js.map
