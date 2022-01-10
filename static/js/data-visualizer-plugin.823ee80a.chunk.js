a16bf2f75e65e11cea3e1b01ef29ec6de95deabb
GitHub <noreply@github.com>
2022-01-10 12:29:40 +0000

    a0add62 fix: adjust z-index so tooltip shows (#2059)

--- static/js/data-visualizer-plugin.823ee80a.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{2911:function(p,s){}}]);
-//# sourceMappingURL=data-visualizer-plugin.823ee80a.chunk.js.map
