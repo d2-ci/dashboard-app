32b43fef97992f91e58872feb1bea68ff31a357d
Philip-Larsen-Donnelly <phil@dhis2.org>
2022-04-05 15:00:34 +0000

    44f1851 chore(release-prepare): cut patch/2.36.10

---
+++ static/js/data-visualizer-plugin.288ef0a7.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{2912:function(p,s){}}]);
+//# sourceMappingURL=data-visualizer-plugin.288ef0a7.chunk.js.map
