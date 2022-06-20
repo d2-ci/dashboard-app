ed36c868f3b1871a571ffcd23617bcc0819600ba
Edoardo Sabadelli <edoardo@dhis2.org>
2022-06-20 14:03:39 +0000

    6389f99 fix: bump interpretations component for DHIS2-12617

---
+++ static/js/data-visualizer-plugin.c39bde67.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{2542:function(p,s){}}]);
+//# sourceMappingURL=data-visualizer-plugin.c39bde67.chunk.js.map
