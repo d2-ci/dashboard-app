242922b44e008253ac7f2dc9661a7c99912d27eb
Jen Jones Arnesen <jennifer@dhis2.org>
2022-08-02 07:46:43 +0000

    9d745a9 fix: lint issues

---
+++ static/js/data-visualizer-plugin.4b97ddeb.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{3154:function(p,s){}}]);
+//# sourceMappingURL=data-visualizer-plugin.4b97ddeb.chunk.js.map
