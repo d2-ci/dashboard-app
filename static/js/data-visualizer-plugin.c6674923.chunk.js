e3599cc6d53a2d43c686319f4ea1371bdfdf29fd
GitHub <noreply@github.com>
2022-09-14 08:47:24 +0000

    f4e7e34 fix: bump internal deps (DHIS2-13298) v38 (#2106)

---
+++ static/js/data-visualizer-plugin.c6674923.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{2566:function(p,s){}}]);
+//# sourceMappingURL=data-visualizer-plugin.c6674923.chunk.js.map
