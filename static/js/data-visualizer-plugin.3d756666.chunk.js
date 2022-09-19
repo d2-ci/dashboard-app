cc4d087a4ed7622fb3066e7cd56efc0a121985f5
GitHub <noreply@github.com>
2022-09-19 08:04:12 +0000

    91be009 fix: bump interpretations component for DHIS2-12617 (#2108)

--- static/js/data-visualizer-plugin.3d756666.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{2493:function(p,s){}}]);
-//# sourceMappingURL=data-visualizer-plugin.3d756666.chunk.js.map
---
+++ static/js/data-visualizer-plugin.8fb6d367.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{2544:function(p,s){}}]);
+//# sourceMappingURL=data-visualizer-plugin.8fb6d367.chunk.js.map
