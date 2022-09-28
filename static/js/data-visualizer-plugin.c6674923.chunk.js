eb7857385ae3c75537700751a94764799e3ef600
GitHub <noreply@github.com>
2022-09-28 13:36:32 +0000

    10b79d3 fix: ItemSelector overflow, orgunit/period filter issues  (DHIS2-13811) v38 (#2122)

--- static/js/data-visualizer-plugin.c6674923.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{2566:function(p,s){}}]);
-//# sourceMappingURL=data-visualizer-plugin.c6674923.chunk.js.map
