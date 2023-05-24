dd10acac4ac1cd742dbb70a37bca3babb46aadfd
Edoardo Sabadelli <edoardo@dhis2.org>
2023-05-24 13:30:32 +0000

    5c2d6fb chore: run prettier

---
+++ static/js/data-visualizer-plugin.e1bdda66.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{2605:function(p,s){}}]);
+//# sourceMappingURL=data-visualizer-plugin.e1bdda66.chunk.js.map
