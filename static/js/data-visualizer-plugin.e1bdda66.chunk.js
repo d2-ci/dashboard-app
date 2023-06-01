a75e98fe51432b04a320f3e8fd59774fd91a2014
Edoardo Sabadelli <edoardo@dhis2.org>
2023-06-01 12:01:25 +0000

    3c5e98d test: fix snapshot

--- static/js/data-visualizer-plugin.e1bdda66.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{2605:function(p,s){}}]);
-//# sourceMappingURL=data-visualizer-plugin.e1bdda66.chunk.js.map
