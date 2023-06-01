baa3eb661d393861004ffdf227632c75298201a0
GitHub <noreply@github.com>
2023-06-01 11:33:28 +0000

    c95c680 fix(deps): bump platform deps to cli 10.3.6 (v38) (#2274)

--- static/js/data-visualizer-plugin.84cc1aa6.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[136],{2596:function(p,s){}}]);
-//# sourceMappingURL=data-visualizer-plugin.84cc1aa6.chunk.js.map
